65da776eda6fea096e0414a98834371e171e6fd2
indra  susila <indra.susila@logique.co.id>
2021-04-03 08:45:20 +0700

    feat: hahahaha

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { ReactElement } from 'react'
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+export default function Index(): ReactElement {
+  return (
+    <div>
+      <h1>=The Next Donaser Boilerplate</h1>
 
-export default IndexPage
+      <style jsx>{`
+        p {
+          color: red;
+        }
+      `}</style>
+    </div>
+  )
+}
